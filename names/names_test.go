09c6048291dd7fab86e45096f39c2b9c61b6548a
ymirthor <ymirthorleifsson@gmail.com>
2021-04-01 17:12:17 +0000

    first test added

--- names/names_test.go
+++ names/names_test.go
@@ -1,1 +1,15 @@
 package names_test
+
+import (
+	. "github.com/bjartur20/T-419-CADP_OverlayNetworks/names"
+	"testing"
+	"fmt"
+)
+
+func TestMake(t *testing.T) {
+	names := Make()
+	namesType := fmt.Sprintf("%T", names)
+	if namesType != "*names.Names" {
+		t.Errorf("have %s want *names.Names", namesType)
+	}
+}
