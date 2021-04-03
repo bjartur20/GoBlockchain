30fa8cf7d263b066af8f73cceec0484e1aecf887
ymirthor <ymirthorleifsson@gmail.com>
2021-04-03 16:43:55 +0000

    100% test coverage on names package

--- names/names.go
+++ names/names.go
@@ -1,35 +1,32 @@
 package names
 
 import (
-	"time"
 	"errors"
+	"math"
+	"time"
 )
 
 type Names struct {
-	names map[string]string
-	heartbeat map[string]int64
-	timeout int64
+	names      map[string]string
+	heartbeats map[string]int64
+	timeout    int64
 }
-
 type Registration struct {
-	name string
+	name    string
 	address string // IP:port
 }
 
-func (n *Names) Register(args *Registration) (res *string, err error) {
+func (n *Names) Register(args *Registration) (err error) {
 	// Not sure if any of the things I am doing here is correct
 	n.names[args.name] = args.address
-	n.heartbeat[args.name] = time.Now().UnixNano() // This is converting time to int64
+	n.heartbeats[args.name] = time.Now().UnixNano() // This is converting time to int64
 	return
 }
-
-func (n *Names) Unregister(args *string) (res *string, err error) {
-	// Remove host from heartbeat and from names
-	delete(n.heartbeat, *args)
+func (n *Names) Unregister(args *string) (err error) {
+	delete(n.heartbeats, *args)
 	delete(n.names, *args)
 	return
 }
-
 func (n *Names) Resolve(args *string) (res *string, err error) {
 	if val, ok := n.names[*args]; ok {
 		res = &val
@@ -38,34 +35,31 @@ func (n *Names) Resolve(args *string) (res *string, err error) {
 	err = errors.New("not found")
 	return
 }
-
 func (n *Names) Heartbeat(args *string) (res int64, err error) {
-	if val, ok := n.heartbeat[*args]; ok {
+	if val, ok := n.heartbeats[*args]; ok {
 		res = val
 		return
 	}
 	err = errors.New("not found")
 	return
 }
-
 func (n *Names) checkHeartbeat() {
 	for {
 		time.Sleep(time.Duration(n.timeout))
 		timeNow := time.Now().UnixNano()
 		// Remove all entries whose heartbeat is older than 60 seconds
-		for host, heartbeat := range n.heartbeat {
-			if heartbeat - timeNow > 60 { // Im not sure if I am using the right unit here
+		for host, heartbeat := range n.heartbeats {
+			if timeNow-heartbeat > 60*int64(math.Pow10(9)) {
 				n.Unregister(&host)
 			}
 		}
 	}
 }
-
 func Make() (res *Names) {
 	res = &Names{
-		names:     make(map[string]string),
-        heartbeat: make(map[string]int64),
-        timeout:   42, // Some base timeout 
+		names:      make(map[string]string),
+		heartbeats: make(map[string]int64),
+		timeout:    int64(math.Pow10(9)), // 1 second base timeout
 	}
 	go res.checkHeartbeat()
 	return
--- names/names_test.go
+++ names/names_test.go
@@ -1,9 +1,10 @@
-package names_test
+package names
 
 import (
-	. "github.com/bjartur20/T-419-CADP_OverlayNetworks/names"
-	"testing"
 	"fmt"
+	"math"
+	"testing"
+	"time"
 )
 
 func TestMake(t *testing.T) {
@@ -11,5 +12,96 @@ func TestMake(t *testing.T) {
 	namesType := fmt.Sprintf("%T", names)
 	if namesType != "*names.Names" {
 		t.Errorf("have %s want *names.Names", namesType)
+	}
+}
+func TestRegister(t *testing.T) {
+	names := Make()
+	node := Registration{"A", "123"}
+	err := names.Register(&node)
+	if err != nil {
+		t.Errorf("Register failed")
+	}
+	if address, ok := names.names[node.name]; ok {
+		if address != node.address {
+			t.Errorf("Did not match input")
+		}
+	} else {
+		t.Error("Was not put into names map")
+	}
+	if hb, ok := names.heartbeats[node.name]; ok {
+		now := time.Now().UnixNano()
+		if hb < now-5*int64(math.Pow10(8)) || hb > now {
+			t.Error("Did not match time")
+		}
+	} else {
+		t.Error("Was not put into heartbeat map")
+	}
+}
+func TestUnregister(t *testing.T) {
+	names := Make()
+	A := Registration{"A", "123"}
+	names.Register(&A)
+	B := Registration{"B", "124"}
+	names.Register(&B)
+	names.Unregister(&A.name)
+	if len(names.names) != 1 {
+		t.Error("Name was not deleted from names map")
+	}
+	if len(names.heartbeats) != 1 {
+		t.Error("Name was not deleted from heartbeats map")
+	}
+}
+func TestResolve(t *testing.T) {
+	names := Make()
+	A := Registration{"A", "123"}
+	names.Register(&A)
+	B := Registration{"B", "124"}
+	names.Register(&B)
+	// Test find it
+	address, err := names.Resolve(&A.name)
+	if err != nil {
+		t.Errorf("Resolve failed: %s", err)
+	}
+	if *address != A.address {
+		t.Error("Did not find correct address")
+	}
+	// Test don't find it
+	name := "C"
+	address, err = names.Resolve(&name)
+	if err == nil {
+		t.Error("Resolve should have failed")
+	} else {
+		if address != nil {
+			t.Error("should not have returned an address")
+		}
+	}
+}
+func TestHeartbeat(t *testing.T) {
+	names := Make()
+	A := Registration{"A", "123"}
+	names.Register(&A)
+	B := Registration{"B", "124"}
+	names.Register(&B)
+	// Test find it
+	_, err := names.Heartbeat(&A.name)
+	if err != nil {
+		t.Errorf("Heartbeat failed: %s", err)
+	}
+	// Test don't find it
+	name := "C"
+	_, err = names.Heartbeat(&name)
+	if err == nil {
+		t.Error("Heartbeat should have failed")
+	}
+}
+func TestCheckHeartbeat(t *testing.T) {
+	names := Make()
+	A := Registration{"Dead heartbeat", "123"}
+	names.Register(&A)
+	names.heartbeats[A.name] = time.Now().UnixNano() - 120*int64(math.Pow10(9))
+
+	time.Sleep(time.Second)
+	if len(names.names) != 0 || len(names.heartbeats) != 0 {
+		t.Error("node should have unregistered")
 	}
 }
